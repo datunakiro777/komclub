1c081df8c791e53650f6d6dc6454b0f5d0af3f76
unknown <davitpapidze777@gmail.com>
2024-12-30 16:33:50 +0400

    did something beka ar sheexo jer vmushaob

--- KomClub/KomClub/urls.py
+++ KomClub/KomClub/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from registration import views as v
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('HomePage.urls'))
+    path('', include('HomePage.urls')),
+    path('register/', v.registration, name='regisration')
 ]
---
+++ KomClub/registration/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
+# Create your views here.
+def registration(request):
+    form = UserCreationForm()
+    return render(request, 'registration/registration.html', {'form':form})
