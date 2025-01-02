0c30f8efe7508cc476e654b60509c3320147545b
ბექა <91470466+bekatar@users.noreply.github.com>
2025-01-02 15:06:52 +0400

    fixed little bugs and added login/register design

--- KomClub/KomClub/settings.py
+++ KomClub/KomClub/settings.py
@@ -17,6 +17,7 @@ import os
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
@@ -121,6 +122,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
