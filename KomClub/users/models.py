19730f2c695417e6693ad816e52ec007e30f42a3
unknown <davitpapidze777@gmail.com>
2025-01-04 14:24:43 +0400

    done fixing

--- KomClub/users/models.py
+++ KomClub/users/models.py
@@ -5,12 +5,6 @@ class User_info(models.Model):
     username = models.CharField(max_length=15, default='bot')
     gmail = models.EmailField(unique=True)
     password = models.CharField(max_length=20, default='bot123')
-    pfp = models.ImageField(default='static/images/defaultpfp.jpg')
-    def set_password(self, raw_password):
-        self.password = make_password(raw_password)
-
-    def check_password(self, raw_password):
-        return check_password(raw_password, self.password)
-
+    pfp = models.ImageField(default='../../static/images/defaultpfp.jpg')
     def __str__(self):
         return self.username
